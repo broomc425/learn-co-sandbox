89823374f02e8c09c1100221d5b97a798a370459
Charlie Broom <broomc425@gmail.com>
2020-03-12 16:12:21 +0000

    Automatically backed up by Learn

---
+++ surf.rb
@@ -0,0 +1,71 @@
+# def surf_jaws_method
+#   local_variable = 'Fish board!'
+#   puts local_variable
+# end
+
+# surf_jaws_method
+
+
+
+# def output_mountains_elements(array)
+#   counter = 0
+  
+# counter = 0
+# mountains = ["Vail", "Breckinridge", "Keystone", "Copper", "Stevens"]
+
+# def output_array_elements(array)
+#   counter = 0
+ 
+#   while array[counter] do
+#     puts array[counter]
+#     counter += 1
+#   end
+# end
+
+# output_array_elements(mountains)
+
+# mountains = {
+#   colorado: ["Vail", "Breckinridge"],
+#   washington: ["Baker", "Stevens"]
+# }
+# mountains
+
+
+# surfers = {
+#   australian: %w[owen matt mick joel]
+#   hawaiian: %w{john ezekiel sebastian seth]
+#   brazilian: %w[gabriel italo filipe adriano]
+# }
+
+# surfers.each{ |pair| p pair }
+# surfers.each_pair{ |pair| p pair }
+# surfers.reduce({}) do |memo, pair|
+#   p memo
+#   p pair
+#   memo
+# end
+# surfers.reduce({}) do |memo, (key, value)|
+#   p memo
+#   p key
+#   p value
+#   memo
+# end
+
+# sorted_member_list = surfers.reduce({}) do |memo, (key, value)|
+#   memo[key] = value.
+#   memo
+# end
+
+# p surfers
+# p sorted_member_list
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
